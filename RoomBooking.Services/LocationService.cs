9f9cf3b63428195ef4f48204d28b531cf9cc8412
Therese Samuelsson <therese.x@gmail.com>
2018-11-17 17:41:35 +0100

    GREEN - LocationServiceUnitTest ServiceCreationWithNullContextShouldFail

--- RoomBooking.Services/LocationService.cs
+++ RoomBooking.Services/LocationService.cs
@@ -9,7 +9,7 @@ namespace RoomBooking.Services
         private DataContext _context;
         public LocationService(DataContext context)
         {
-            this._context = context;
+            _context = context ?? throw new ArgumentNullException();
         }
     }
 }
