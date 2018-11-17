8c5f0587d6beb0bc436ac97655ebabf737a7c51f
Therese Samuelsson <therese.x@gmail.com>
2018-11-17 14:39:47 +0100

    GREEN - LocationUnitTest ShouldThrowErrorOnEmptyName

--- RoomBooking.Core/Models/Location.cs
+++ RoomBooking.Core/Models/Location.cs
@@ -8,7 +8,7 @@ namespace RoomBooking.Core.Models
     public class Location
     {
         public int ID { get; set; }
-        
+        [Required]
         public string Name { get; set; }
     }
 }
