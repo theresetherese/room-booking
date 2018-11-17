f176edd731b4523e32b21ab94b0e56c029531616
Therese Samuelsson <therese.x@gmail.com>
2018-11-17 14:55:26 +0100

    New test project for Services

---
+++ RoomBooking.Services.Tests/LocationServiceUnitTest.cs
@@ -0,0 +1,14 @@
+using System;
+using Xunit;
+
+namespace RoomBooking.Services.Tests
+{
+    public class LocationServiceUnitTest
+    {
+        [Fact]
+        public void Test1()
+        {
+
+        }
+    }
+}
