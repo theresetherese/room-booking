4cd42f508e37927c45f802e980792015db2818ad
Therese Samuelsson <therese.x@gmail.com>
2018-11-28 21:31:26 +0100

    GREEN - Room model, location validation

--- RoomBooking.Core/Models/Room.cs
+++ RoomBooking.Core/Models/Room.cs
@@ -10,6 +10,7 @@ namespace RoomBooking.Core.Models
         public int ID { get; set; }
         [Required]
         public string Name { get; set; }
+        [Required]
         public Location Location { get; set; }
     }
 }
