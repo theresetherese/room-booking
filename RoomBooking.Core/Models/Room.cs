77820e0f04b475a92f301fefa8ad3e6404988f19
Therese Samuelsson <therese.x@gmail.com>
2018-11-28 21:24:07 +0100

    GREEN - Room model, name validation

--- RoomBooking.Core/Models/Room.cs
+++ RoomBooking.Core/Models/Room.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Text;
 
 namespace RoomBooking.Core.Models
@@ -7,6 +8,7 @@ namespace RoomBooking.Core.Models
     public class Room
     {
         public int ID { get; set; }
+        [Required]
         public string Name { get; set; }
         public int LocationId { get; set; }
         public Location Location { get; set; }
