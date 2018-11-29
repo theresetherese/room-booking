e9c74b3c773169048847feecf67a5c6d0d65d704
Therese Samuelsson <therese.x@gmail.com>
2018-11-29 23:53:35 +0100

    GREEN - RoomService constructor null context validation

--- RoomBooking.Services/RoomService.cs
+++ RoomBooking.Services/RoomService.cs
@@ -13,7 +13,7 @@ namespace RoomBooking.Services
         private DataContext _context;
         public RoomService(DataContext context)
         {
-            _context = context;
+            _context = context ?? throw new ArgumentNullException(nameof(context));
         }
         public async Task<IEnumerable<Room>> GetRoomsByLocation(int locationId)
         {
