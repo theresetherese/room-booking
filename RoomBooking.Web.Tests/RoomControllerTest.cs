15549ec89e3d46aa36ec90a7ab63722cdf9e73b2
Therese Samuelsson <therese.x@gmail.com>
2018-11-30 10:15:13 +0100

    RED - RoomController constructor (switching to gui first rather than db first)

---
+++ RoomBooking.Web.Tests/RoomControllerTest.cs
@@ -0,0 +1,21 @@
+﻿using Moq;
+using RoomBooking.Core.Interfaces;
+using RoomBooking.Web.Controllers;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using Xunit;
+
+namespace RoomBooking.Web.Tests
+{
+    public class RoomControllerTest
+    {
+        [Fact]
+        public void Constructor_OK()
+        {
+            var mockRoomService = new Mock<IRoomService>();
+            new RoomController(mockRoomService.Object);
+        }
+
+    }
+}
---
+++ RoomBooking.Web/Controllers/RoomController.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using RoomBooking.Core.Interfaces;
+
+namespace RoomBooking.Web.Controllers
+{
+    public class RoomController : Controller
+    {
+        public RoomController(IRoomService roomService)
+        {
+            throw new NotImplementedException();
+        }
+        public async Task<IActionResult> Index()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
