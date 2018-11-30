5c75bb5f38ea8274987d52ff9fbb538164a5c870
Therese Samuelsson <therese.x@gmail.com>
2018-11-30 10:45:30 +0100

    GREEN - RoomController Index return Error when room not found

--- RoomBooking.Web/Controllers/RoomController.cs
+++ RoomBooking.Web/Controllers/RoomController.cs
@@ -1,9 +1,11 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Diagnostics;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using RoomBooking.Core.Interfaces;
+using RoomBooking.Web.Models;
 using RoomBooking.Web.Models.Room;
 
 namespace RoomBooking.Web.Controllers
@@ -19,10 +21,18 @@ namespace RoomBooking.Web.Controllers
         {
             var room = await _roomService.GetRoom(id);
 
+            if (room == null)
+                return Error();
+
             IndexViewModel vm = new IndexViewModel();
             vm.Room = room.Name;
 
             return View(vm);
+        }
+
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext?.TraceIdentifier });
         }
     }
 }
